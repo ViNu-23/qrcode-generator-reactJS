e91fd7568c7bb2d3602b0847fc9dc5105174d25e
ViNu-23 <vijayvinu46@gmail.com>
2023-12-17 23:43:01 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import './index.css';
+import ReactDOM from 'react-dom/client';
 
+function Main() {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Main />);
