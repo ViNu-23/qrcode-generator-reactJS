0397835d6e7a20446a1c96d587cc6f51b6eaaa5c
ViNu-23 <vijayvinu46@gmail.com>
2023-12-20 21:31:06 +0530

    ux-template

--- src/index.js
+++ src/index.js
@@ -1,14 +1,43 @@
-import React from 'react';
-import './index.css';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  Heading,
+  Button,
+  Input,
+  Container,
+  ChakraProvider,
+  Center,
+} from "@chakra-ui/react";
 
 function Main() {
   return (
     <>
-      <h1>Hello</h1>
+      <div className="container">
+        <ChakraProvider>
+          <Center h="100vh">
+            <Card align="center" boxShadow="2xl" bg="white" m={10} p={10}>
+              <CardHeader>
+                <Heading color="#2B6CB0">QR Code Generator</Heading>
+              </CardHeader>
+              <Container>
+                <Input variant="outline" placeholder="Type your url" mt={10} />
+              </Container>
+              <CardFooter>
+                <Button colorScheme="blue" mt={8} size="lg">
+                  Generate
+                </Button>
+              </CardFooter>
+            </Card>
+          </Center>
+        </ChakraProvider>
+      </div>
     </>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Main />);
